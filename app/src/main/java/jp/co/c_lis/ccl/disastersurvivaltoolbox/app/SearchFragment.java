1f547b135c99e3ed5679b1343727e2a1ec13e16a
Keiji Ariyama <keiji_ariyama@c-lis.co.jp>
2014-05-02 01:53:13 +0900

    検索画面のデザイン原案を実装した。 DisasterTypeアイコンを待って実装を進める。

--- app/src/main/java/jp/co/c_lis/ccl/disastersurvivaltoolbox/app/SearchFragment.java
+++ app/src/main/java/jp/co/c_lis/ccl/disastersurvivaltoolbox/app/SearchFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class SearchFragment extends AbsFragment<SearchFragmentListener> {
 
@@ -22,9 +24,47 @@ public class SearchFragment extends AbsFragment<SearchFragmentListener> {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(getTitle());
+
+        mDisasterTypeAdapter = new DisasterTypeAdapter();
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.gv_disaster_types);
+        gridView.setAdapter(mDisasterTypeAdapter);
         return rootView;
+    }
+
+    private DisasterTypeAdapter mDisasterTypeAdapter;
+
+    private static final int[] DISASTER_TYPES = new int[]{
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+    };
+
+    private class DisasterTypeAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return DISASTER_TYPES.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return DISASTER_TYPES[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setImageResource(((Integer) getItem(position)));
+            return imageView;
+        }
     }
 
     @Override
